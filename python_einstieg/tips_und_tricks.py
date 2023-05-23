0d4daa5d286f373b0b275600facf8d6561f5406c
Valentin <valentin.bystrich@nfc21.de>
2023-05-23 18:02:18 +0200

    tips added

---
+++ python_einstieg/tips_und_tricks.py
@@ -0,0 +1,32 @@
+# Zwei Möglichkeiten hochzuzählen:
+
+zahl = 1
+
+zahl += 1
+
+zahl == zahl + 1
+
+# Länge von einem String oder von einer Liste
+
+länge_1 = len("Hallo")
+
+liste = [1, 4, 7, 8]
+
+länge_2 = len(liste)
+
+# Kleiner und größer
+
+print (3<4)
+
+print (3>4)
+
+# Arbeiten mit Listen
+meine_neue_liste = [1, 10, 100]
+
+meine_neue_liste.pop(0) # löscht den ersten Eintrag
+
+meine_neue_liste.remove(10) # löscht die erste 10
+
+meine_neue_liste.append(1000) # fügt eine 1000 hinzu
+
+meine_neue_liste
