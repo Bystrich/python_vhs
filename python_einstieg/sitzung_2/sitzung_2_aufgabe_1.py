4a5559d1d70ce802126654b8bdd5b66d9499ea76
Valentin <valentin.bystrich@nfc21.de>
2023-09-28 18:09:54 +0200

    sitzung 2 added

--- python_einstieg/sitzung_2/sitzung_2_aufgabe_1.py
+++ python_einstieg/sitzung_2/sitzung_2_aufgabe_1.py
@@ -4,18 +4,24 @@ neue_nummer = int(input("Bitte geben sie eine nummer ein: "))
 if neue_nummer > 0:
     if neue_nummer % 2 == 0:
         print("Die Nummer ist gerade")
-else:
-    print("Die Nummer ist ungerade")
+    else:
+        print("Die Nummer ist ungerade")
 else:
     print("Die Nummer ist null oder negativ")
 
 
 # Schreibe Code der nach dem Alter des Benutzers fragt und durch EINE If-Anweisung überprüft ob das genannte Alter sinnvoll ist (z.B. zwischen 5 und 100 Jahren)
 
+alter = int(input("Wie alt bist du?"))
 
+if alter > 5 and alter < 100:
+    print("das ist ein realistisches alter")
 
 
 # Ändere den Code so, dass die If-Anweisung wahr ist, wenn die Zahl nicht zwischen 5 und 8 liegt. Tip: dafür muss nur ein Wort hinzugefügt werden!
 nummer = int(input("Bitte geben sie eine nummer ein: "))
-if (number >= 5 and number <= 8):
+if (not(nummer >= 5 and nummer <= 8)):
+    print("Die eingebene nummer ist nicht zwischen 5 und 8")
+
+if (nummer <= 5 or nummer >= 8):
     print("Die eingebene nummer ist nicht zwischen 5 und 8")
