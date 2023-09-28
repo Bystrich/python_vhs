9e06ea880e20b0f6a821165a5277896a126d3bd5
Valentin <valentin.bystrich@nfc21.de>
2023-09-28 18:28:48 +0200

    sitzung 2 added

---
+++ python_einstieg/sitzung_3/sitzung_3_aufgabe_2.py
@@ -0,0 +1,3 @@
+# Schreibe ein kleines Programm, dass den Nutzer an die Einkaufliste weitere Dinge hinzufügen lässt. Dazu soll erst gefragt werden wie viele Dinge hinzugefügt werden sollen. Dannach soll entsprechend oft nach dem neuen Gegenstand gefragt werden.
+
+meine_einkaufsliste = ["Apfel", "Birne", "Fender Telecaster"]
