a507772dd21a54e19cdbd60d7d0e3a4e1caacf93
Valentin <valentin.bystrich@nfc21.de>
2023-09-29 08:05:03 +0200

    update after meeting 3

--- python_einstieg/sitzung_2/sitzung_2_aufgabe_3.py
+++ python_einstieg/sitzung_2/sitzung_2_aufgabe_3.py
@@ -1,14 +1,21 @@
 # Schreibe eine Funktion die ein Quadrat beliebiger Größe aus einem gewählten Zeichen ausgibt
 
 def quadrat_aus_zeichen(groesse, zeichen):
-  print("Hierhin kommt dein Code")
+  reihe = 1
+
+  while reihe <= groesse:
+    print(groesse * zeichen)
+    reihe = reihe + 1
 
 # Schreibe eine Funktion die überprüft ob zwei bestimmte Buchstaben in einem Wort gleich sind. So sollte z.B. print(gleiche_buchstaben("programmierer", 6, 7)) True ausgeben
 
 def gleiche_buchstaben(wort, b_1, b_2):
-  return False
+  antwort = wort[b_1] == wort[b_2]
+  return antwort
 
 
-quadrat_aus_zeichen(4, "%")
+#quadrat_aus_zeichen(10, "a")
+#quadrat_aus_zeichen(12, "p")
 
 print (gleiche_buchstaben("test", 0 , 3))
+print (gleiche_buchstaben("Valentin", 4 , 6))
---
+++ python_einstieg/sitzung_3/sitzung_3_beispiel.py
@@ -0,0 +1,29 @@
+# meine_liste = ["Vali", "Martin"]
+#
+
+#
+# meine_zweite_liste.pop(0)
+#
+# meine_zweite_liste.remove(10)
+#
+# meine_liste.append("Tobias")
+#
+# print(meine_liste[1])
+#
+# print(meine_zweite_liste)
+#
+# summe = meine_zweite_liste[0] + meine_zweite_liste[2]
+#
+# print(summe)
+
+meine_zweite_liste = [36, 4, 10, 15, 23, 14, 10]
+
+meine_einkäufe = ["Apfel", "Birne", "Messer"]
+
+for j in [1,2,3,4,5,6,7,8,9]:
+    ausgabe = 10 * j
+    print(ausgabe)
+
+for i in range(0,3):
+    print("Hallo")
+
