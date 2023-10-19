7d51bb77f50f5435cb0d43ac1f918a8bc1e06520
Valentin <valentin.bystrich@nfc21.de>
2023-10-19 19:06:18 +0200

    sitzung 4 added

--- python_einstieg/rekursion/rekursion_aufgabe.py
+++ python_einstieg/rekursion/rekursion_aufgabe.py
@@ -1,6 +1,6 @@
 # Definiere eine rekursive Funktion die n-te Fibonacci Nummer ausgibt (schwer)
 def fibonacci(n):
-    if n <= 1:
+    if n <= 2:
         return n
     else:
         return (fibonacci(n-1) + fibonacci(n-2))
